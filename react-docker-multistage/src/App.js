d5ca0db3f1b339b3cc0c9e632cd5863a2ba73121
Madhvi Salaria <anub6217@gmail.com>
2025-11-06 18:51:44 +0530

    Create App component for React Docker app

---
+++ react-docker-multistage/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>🚀 React App Dockerized with Multi-Stage Build</h1>
+      <p>This app is served using NGINX inside a Docker container!</p>
+    </div>
+  );
+}
+
+export default App;
